6d6ac52a0a2c47e8a316ed503f737d810f80fe8a
landy <14ndy15@gmail.com>
2018-07-02 10:50:13 -0400

    chage site title and added hotjar tracking code

--- public_html/app_dev.php
+++ public_html/app_dev.php
@@ -20,7 +20,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
-
 require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
