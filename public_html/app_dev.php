37e912c663b0252d45896b0caecef0b4482af7a9
Josué <josmiguel92@gmail.com>
2018-07-03 16:18:08 +0200

    first dev commit

--- public_html/app_dev.php
+++ public_html/app_dev.php
@@ -11,7 +11,7 @@ umask(0000);
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 
-
+/*
 if (isset($_SERVER['HTTP_CLIENT_IP'])
     || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
     || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) || PHP_SAPI === 'cli-server')
@@ -19,7 +19,7 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
