55490dc54a2374ac3bba53abe51ef6c96eb0dfa9
landy <14ndy15@gmail.com>
2018-07-18 18:38:09 -0400

    change quality of images

--- src/AppBundle/Entity/ImageField.php
+++ src/AppBundle/Entity/ImageField.php
@@ -240,7 +240,7 @@ class ImageField
             $new_width, $new_height,
             $width, $height);
 
-			@imagejpeg($thumb, $filenames[$i], 85);
+			@imagejpeg($thumb, $filenames[$i], 70);
 
 
 		}
