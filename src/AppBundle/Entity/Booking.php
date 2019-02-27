d546bae929b4ba8b028966bbb6ee7aa444d13094
josmiguel92 <josmiguel92@gmail.com>
2019-02-27 10:54:22 -0500

    adding TRUE entropy to token of booking

--- src/AppBundle/Entity/Booking.php
+++ src/AppBundle/Entity/Booking.php
@@ -435,7 +435,7 @@ class Booking
 
     function __construct()
     {
-        $this->token =  "TDC-".date("d/m")."-".substr(uniqid("", true),8,8);
+        $this->token =  "TDC-".date("d.m")."-".substr(uniqid("", true),8,8);
         $this->ownroute = new ArrayCollection();
         $this->setAccepted(false);
         $this->setConfirmed(false);
